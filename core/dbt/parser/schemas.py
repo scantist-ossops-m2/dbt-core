18df024284db204bb629167c493cedbf23fe1581
Gerda Shank <gerda@dbtlabs.com>
2024-06-28 14:55:11 -0400

    12 mypy error remaining

--- core/dbt/parser/schemas.py
+++ core/dbt/parser/schemas.py
@@ -6,9 +6,11 @@ from typing import Any, Callable, Dict, Generic, Iterable, List, Optional, Type,
 
 from dbt import deprecations
 from dbt.clients.yaml_helper import load_yaml_text
+from dbt.config import RuntimeConfig
 from dbt.context.configured import SchemaYamlVars, generate_schema_yml_context
 from dbt.context.context_config import ContextConfig
-from dbt.contracts.files import SchemaSourceFile
+from dbt.contracts.files import SchemaSourceFile, SourceFile
+from dbt.contracts.graph.manifest import Manifest
 from dbt.contracts.graph.nodes import (
     ModelNode,
     ParsedMacroPatch,
@@ -142,9 +144,9 @@ def yaml_from_file(source_file: SchemaSourceFile) -> Optional[Dict[str, Any]]:
 class SchemaParser(SimpleParser[YamlBlock, ModelNode]):
     def __init__(
         self,
-        project,
-        manifest,
-        root_project,
+        project: RuntimeConfig,
+        manifest: Manifest,
+        root_project: RuntimeConfig,
     ) -> None:
         super().__init__(project, manifest, root_project)
 
@@ -282,33 +284,33 @@ class ParseResult:
 # PatchParser, SemanticModelParser, SavedQueryParser, UnitTestParser
 class YamlReader(metaclass=ABCMeta):
     def __init__(self, schema_parser: SchemaParser, yaml: YamlBlock, key: str) -> None:
-        self.schema_parser = schema_parser
+        self.schema_parser: SchemaParser = schema_parser
         # key: models, seeds, snapshots, sources, macros,
         # analyses, exposures, unit_tests
-        self.key = key
-        self.yaml = yaml
-        self.schema_yaml_vars = SchemaYamlVars()
+        self.key: str = key
+        self.yaml: YamlBlock = yaml
+        self.schema_yaml_vars: SchemaYamlVars = SchemaYamlVars()
         self.render_ctx = generate_schema_yml_context(
             self.schema_parser.root_project,
             self.schema_parser.project.project_name,
             self.schema_yaml_vars,
         )
-        self.renderer = SchemaYamlRenderer(self.render_ctx, self.key)
+        self.renderer: SchemaYamlRenderer = SchemaYamlRenderer(self.render_ctx, self.key)
 
     @property
-    def manifest(self):
+    def manifest(self) -> Manifest:
         return self.schema_parser.manifest
 
     @property
-    def project(self):
+    def project(self) -> RuntimeConfig:
         return self.schema_parser.project
 
     @property
-    def default_database(self):
+    def default_database(self) -> str:
         return self.schema_parser.default_database
 
     @property
-    def root_project(self):
+    def root_project(self) -> RuntimeConfig:
         return self.schema_parser.root_project
 
     # for the different schema subparsers ('models', 'source', etc)
@@ -425,7 +427,9 @@ class SourceParser(YamlReader):
                 fqn=fqn,
                 name=f"{source.name}_{table.name}",
             )
-            self.manifest.add_source(self.yaml.file, source_def)
+            assert isinstance(self.yaml.file, SchemaSourceFile)
+            source_file: SchemaSourceFile = self.yaml.file
+            self.manifest.add_source(source_file, source_def)
 
 
 # This class has two subclasses: NodePatchParser and MacroPatchParser
@@ -545,15 +549,15 @@ class PatchParser(YamlReader, Generic[NonSourceTarget, Parsed]):
         return self.normalize_attribute(data, path, "access")
 
     @property
-    def is_root_project(self):
+    def is_root_project(self) -> bool:
         if self.root_project.project_name == self.project.project_name:
             return True
         return False
 
-    def validate_data_tests(self, data):
+    def validate_data_tests(self, data) -> None:
         # Rename 'tests' -> 'data_tests' at both model-level and column-level
         # Raise a validation error if the user has defined both names
-        def validate_and_rename(data, is_root_project: bool):
+        def validate_and_rename(data, is_root_project: bool) -> None:
             if data.get("tests"):
                 if "tests" in data and "data_tests" in data:
                     raise ValidationError(
@@ -583,7 +587,7 @@ class PatchParser(YamlReader, Generic[NonSourceTarget, Parsed]):
                     for column in version["columns"]:
                         validate_and_rename(column, self.is_root_project)
 
-    def patch_node_config(self, node, patch):
+    def patch_node_config(self, node, patch) -> None:
         if "access" in patch.config:
             if AccessType.is_valid(patch.config["access"]):
                 patch.config["access"] = AccessType(patch.config["access"])
@@ -784,7 +788,7 @@ class ModelPatchParser(NodePatchParser[UnparsedModelUpdate]):
                     versioned_model_name, target.package_name, None
                 )
 
-                versioned_model_node = None
+                versioned_model_node: ModelNode
                 add_node_nofile_fn: Callable
 
                 # If this is the latest version, it's allowed to define itself in a model file name that doesn't have a suffix
@@ -808,12 +812,17 @@ class ModelPatchParser(NodePatchParser[UnparsedModelUpdate]):
                                 "in `dbt_project.yml` or in the sql files."
                             )
                             raise ParsingError(msg)
-                        versioned_model_node = self.manifest.disabled.pop(
-                            found_nodes[0].unique_id
-                        )[0]
+                        # We know that there's only one node in the disabled list because
+                        # otherwise we would have raised the error above
+                        found_node = found_nodes[0]
+                        self.manifest.disabled.pop(found_node.unique_id)
+                        assert isinstance(found_node, ModelNode)
+                        versioned_model_node = found_node
                         add_node_nofile_fn = self.manifest.add_disabled_nofile
                 else:
-                    versioned_model_node = self.manifest.nodes.pop(versioned_model_unique_id)
+                    found_node = self.manifest.nodes.pop(versioned_model_unique_id)
+                    assert isinstance(found_node, ModelNode)
+                    versioned_model_node = found_node
                     add_node_nofile_fn = self.manifest.add_node_nofile
 
                 if versioned_model_node is None:
@@ -832,12 +841,10 @@ class ModelPatchParser(NodePatchParser[UnparsedModelUpdate]):
                     f"model.{target.package_name}.{target.name}.{unparsed_version.formatted_v}"
                 )
                 # update source file.nodes with new unique_id
-                self.manifest.files[versioned_model_node.file_id].nodes.remove(
-                    versioned_model_node_unique_id_old
-                )
-                self.manifest.files[versioned_model_node.file_id].nodes.append(
-                    versioned_model_node.unique_id
-                )
+                model_source_file = self.manifest.files[versioned_model_node.file_id]
+                assert isinstance(model_source_file, SourceFile)
+                model_source_file.nodes.remove(versioned_model_node_unique_id_old)
+                model_source_file.nodes.append(versioned_model_node.unique_id)
 
                 # update versioned node fqn
                 versioned_model_node.fqn[-1] = target.name
